96cf3f2b7ae036a58a2aaa8cadb98f69c75da82e
Pentorabot <208218034+pentorabot@users.noreply.github.com>
2025-10-09 00:20:56 +0300

    fix(docs): update production URL and project name in config file

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://pentora.ai',
+  url: 'https://docs.pentora.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -23,7 +23,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'pentora', // Usually your GitHub org/user name.
-  projectName: 'pentora', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
